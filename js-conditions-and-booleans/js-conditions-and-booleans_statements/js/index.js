5c189126bd87d15052c572106aeee45a5cf68906
Rita <rita.kenji@gmail.com>
2025-08-01 14:00:33 +0200

    work part 1-3, 3 still unfinished

--- js-conditions-and-booleans/js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_statements/js/index.js
@@ -2,14 +2,65 @@ console.clear();
 
 // Part 1: Password
 const SUPER_SECRET_PASSWORD = "h4x0r1337";
-
 const receivedPassword = "password1234";
+
+const message =
+  SUPER_SECRET_PASSWORD !== receivedPassword
+    ? "Access denied!"
+    : "Welcome! You are logged in as Brunhilde1984.";
+console.log(message);
 
 // Part 2: Even / Odd
 const number = 6;
+let evenOrOdd = number % 2;
+
+if (evenOrOdd == 0) {
+  /* 
+    > Question: Why does Prettier correct it to double (()) when in other instances only one set of () is necessary?
+    > Answer: Initially I wrote (evenOrOdd = 0) whis is incorrect because I am trying to compare values, not reassign:
+        1 - = is to assign a value to a variable,
+        2 - == to compare values, 
+        3 -=== strict compare value and type.
+      And with ´evenOrOdd = 0' Prettier corrected me and put double parenthesis on it so: '((evenOrOdd = 0))', but in other exercises that Gimena made in class (ex: number > 3), only one set of parenthesis was used. Why? Because with == its a simple comparison operation and with = the compiler needs to be told to firstly do that assignment operation before the if condition that follows.
+    */
+  console.log(number, " is an even number");
+} else {
+  console.log(number, " is an odd number");
+}
 
 // Part 3: Hotdogs
 const numberOfHotdogs = 42;
+const justFive = numberOfHotdogs > 5; // justFive will be true or false.
+const fiveToOneHundred = numberOfHotdogs >= 5 && numberOfHotdogs < 100;
+const oneHundredToOneMil = numberOfHotdogs >= 100 && numberOfHotdogs < 1000000;
+const oneMil = numberOfHotdogs >= 1000000;
+let price;
+
+console.log("justFive: ", justFive);
+
+/////////////////////////////////////
+if (numberOfHotdogs > 5) {
+  price = 2;
+}
+//        OR.
+if (justFive) {
+  price = 2;
+}
+////////////////////////////////////
+
+if (justFive) {
+  price = 2;
+} else if (numberOfHotdogs == fiveToOneHundred) {
+  price = 1.5;
+} else if (numberOfHotdogs == oneHundredToOneMil) {
+  price = 1;
+} else if (numberOfHotdogs == oneMil) {
+  price = 0.1;
+} else {
+  price = 0;
+}
+
+console.log("Price: ", price, "€");
 
 // Part 4: Daytime
 const currentHour = 12;
