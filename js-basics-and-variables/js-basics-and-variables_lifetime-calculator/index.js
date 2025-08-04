529c2827c6d8495f83a04dc098e928d6223b223c
Rita <rita.kenji@gmail.com>
2025-08-04 18:05:58 +0200

    calculate everything minus days asleep

--- js-basics-and-variables/js-basics-and-variables_lifetime-calculator/index.js
+++ js-basics-and-variables/js-basics-and-variables_lifetime-calculator/index.js
@@ -18,5 +18,23 @@ Hint: You can combine text and variables using the `+` operator.
 */
 
 const currentAge = Number(process.argv[2]); // This takes the `<age>` entered in the command `node index.js <age>`.
-
 console.log("Your current age is: " + currentAge);
+
+/*The total number of days the person has lived.*/
+let daysLived = currentAge * 365;
+console.log("Number of days lived so far: " + daysLived + " days");
+
+/*The estimated number of remaining days, assuming an average life span of 80 years.*/
+let daysEighty = 80 * 365;
+let daysLeft = daysEighty - daysLived;
+console.log("Number of days left: " + daysLeft + " days");
+
+/*The percentage of life already lived, in relation to the expected life span.*/
+let percentageLifeLived = (currentAge * 100) / 80;
+console.log("Percentage of life already lived: " + percentageLifeLived + "%");
+
+/*The cumulative number of days the person has spent sleeping, assuming an average of 8 hours of sleep per day.*/
+let hoursAsleep = daysLived * 24;
+console.log("Hours asleep: " + hoursAsleep + " hours.");
+
+let daysAsleep = 0;
