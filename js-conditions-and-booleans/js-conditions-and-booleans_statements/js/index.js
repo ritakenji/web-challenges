fbfb938e18f915535eaae2692c61b2f40e11c3dd
Rita <rita.kenji@gmail.com>
2025-08-09 12:26:26 +0200

    added Florian's suggestions

--- js-conditions-and-booleans/js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_statements/js/index.js
@@ -10,6 +10,8 @@ const message =
     : "Welcome! You are logged in as Brunhilde1984.";
 console.log(message);
 
+//--------------------------------------------------X-----------------------------------------------------------------------------
+
 // Part 2: Even / Odd
 const number = 6;
 let evenOrOdd = number % 2;
@@ -19,8 +21,8 @@ if (evenOrOdd === 0) {
     > Question: Why does Prettier correct it to double (()) when in other instances only one set of () is necessary?
     > Answer: Initially I wrote (evenOrOdd = 0) whis is incorrect because I am trying to compare values, not reassign:
         1 - = is to assign a value to a variable,
-        2 - == to compare values, 
-        3 -=== strict compare value and type.
+        2 - == to compare values, < NEVER USE THESE !!!
+        3 -=== strict compare value and type. < USE THESE INSTEAD
       And with ´evenOrOdd = 0' Prettier corrected me and put double parenthesis on it so: '((evenOrOdd = 0))', but in other exercises that Gimena made in class (ex: number > 3), only one set of parenthesis was used. Why? Because with == its a simple comparison operation and with = the compiler needs to be told to firstly do that assignment operation before the if condition that follows.
     */
   console.log(number, " is an even number");
@@ -28,28 +30,31 @@ if (evenOrOdd === 0) {
   console.log(number, " is an odd number");
 }
 
+/* ******************************************* FLORIAN'S TAKE: ************************************************
+if (number % 2 === 0) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
+*/
+
+//--------------------------------------------------X-----------------------------------------------------------------------------
+
 // Part 3: Hotdogs
+
+/* *********************************      HOW I DID IT INITIALLY:     ************************************************
+
 const numberOfHotdogs = 42;
-const justFive = numberOfHotdogs > 5; // justFive will be true or false.
+const justFive = numberOfHotdogs < 5; // >>> justFive will be true or false, written like this makes it a boolean.
 const fiveToOneHundred = numberOfHotdogs >= 5 && numberOfHotdogs < 100;
 const oneHundredToOneMil = numberOfHotdogs >= 100 && numberOfHotdogs < 1000000;
 const oneMil = numberOfHotdogs >= 1000000;
 let price;
 
-console.log("justFive: ", justFive);
-
-/////////////////////////////////////
-if (numberOfHotdogs > 5) {
-  price = 2;
-}
-//        OR.
-if (justFive) {
-  price = 2;
-}
-////////////////////////////////////
+console.log("justFive: ", justFive); // you'll it printing "true"
 
 if (justFive) {
-  price = 2 * numberOfHotdogs;
+  price = 2 *numberOfHotdogs; // this  isn't efficient, also because I'm comparing a boolean with a number. Can also write 'if (numberOfHotdogs < 5) {price = 2;}'
 } else if (numberOfHotdogs === fiveToOneHundred) {
   price = 1.5 * numberOfHotdogs;
 } else if (numberOfHotdogs === oneHundredToOneMil) {
@@ -60,7 +65,29 @@ if (justFive) {
   price = 0;
 }
 
-console.log("Price: ", price, "€");
+console.log("Price: ", price, "€"); */
+
+/* *********************************      HOW FLORIAN SUGGESTED:     ************************************************ */
+
+const numberOfHotdogs = 9;
+
+let totalPrice;
+
+if (numberOfHotdogs < 5) {
+  totalPrice = numberOfHotdogs * 2;
+} else if (numberOfHotdogs < 100) {
+  totalPrice = numberOfHotdogs * 1.5;
+} else if (numberOfHotdogs < 1000000) {
+  totalPrice = numberOfHotdogs * 1;
+} else {
+  totalPrice = numberOfHotdogs * 0.1;
+}
+
+const formattedPrice = totalPrice.toFixed(2); // toFixed(2): returns nr as a string with 2 decimal places. So 2.005.toFixed(2) would return "2.00".
+
+console.log(`total price: ${formattedPrice} €`); // This will insert the value of the variable into the string.
+
+//--------------------------------------------------X-----------------------------------------------------------------------------
 
 // Part 4: Daytime
 const currentHour = 12;
@@ -69,9 +96,19 @@ const statement = currentHour < 17 ? "Still need to learn ..." : "Partytime!!!";
 
 console.log(statement);
 
+//--------------------------------------------------X-----------------------------------------------------------------------------
+
 // Part 5: Greeting
 const userName = "Archibald";
 const coachName = "Gimena";
 const greeting = "Hello " + (userName === coachName ? "coach" : userName) + "!";
 
 console.log(greeting);
+
+/* *********************************      HOW FLORIAN SUGGESTED:     ************************************************ 
+const name = "Jessica";
+
+const greeting = "Hello " + (name === "Jessica" ? "Coach" : name) + "!";
+
+console.log(greeting);
+*/
