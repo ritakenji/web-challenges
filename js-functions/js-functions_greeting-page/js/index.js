8dfc59a9cbacd7b83c8635eb8b91052a463b8b26
Rita <rita.kenji@gmail.com>
2025-08-11 13:18:09 +0200

    finish greting page

--- js-functions/js-functions_greeting-page/js/index.js
+++ js-functions/js-functions_greeting-page/js/index.js
@@ -23,11 +23,30 @@ Update the content and style of the page based on the current day and time.
 const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
-  // Code here
+  let now = new Date().getHours(); // instead of writing new Date().getHours() a million times just make a variable for it
+
+  if (now >= 6 && now <= 12) {
+    console.log("Good Morning");
+  } else if (now >= 13 && now <= 18) {
+    console.log("Good Afternoon");
+  } else if (now >= 19 && now <= 22) {
+    console.log("Good Evening");
+  } else if (now >= 23 && now <= 5) {
+    console.log("Good Night");
+  }
 }
 
+// !!!!!!!!!!!!!!!!    getDay() returns the day of the week where 0 represents Sunday, Monday 1, Tuesday 2 etc...   !!!!!!!!!!!!!!!!!!!!
 function getDayColor() {
-  // Code here
+  let today = new Date().getDay(); // instead of writing new Date().getDay() a million times just make a variable for it
+
+  if (today === 1) {
+    console.log("darkgray");
+  } else if (today >= 2 && today <= 5) {
+    console.log("lightblue");
+  } else if (today >= 6 && today <= 0) {
+    console.log("hot pink");
+  }
 }
 
 display.textContent = getGreeting();
