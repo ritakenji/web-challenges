006b59584d93aa3f3db9d223db7631eff7349010
Rita <rita.kenji@gmail.com>
2025-08-12 12:02:52 +0200

    finish multiple input challenge

--- js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -6,6 +6,10 @@ const copyButton = document.querySelector("[data-js=copy-button]");
 
 // --v-- Write your code here --v--
 
+copyButton.addEventListener("click", () => {
+  secondCopyInput.value = firstCopyInput.value;
+});
+
 // --^-- End of Task 1 --^--
 
 // Task 2: Copy and Uppercase
@@ -22,6 +26,11 @@ const copyInUppercaseButton = document.querySelector(
 
 // --v-- Write your code here --v--
 
+copyInUppercaseButton.addEventListener("click", () => {
+  secondCopyInUppercaseInput.value =
+    firstCopyInUppercaseInput.value.toUpperCase();
+});
+
 // --^-- End of Task 2 --^--
 
 // Task 3: Switch Values
@@ -37,5 +46,27 @@ const switchValueButton = document.querySelector(
 );
 
 // --v-- Write your code here --v--
+
+switchValueButton.addEventListener("click", () => {
+  /*
+  const first = firstSwitchValueInput.value;
+  const second = secondSwitchValueInput.value;
+  secondSwitchValueInput.value = first;
+  firstSwitchValueInput.value = second;
+  // ^ why does this work */
+
+  let temp = firstSwitchValueInput.value; // preserve the value
+  firstSwitchValueInput.value = secondSwitchValueInput.value;
+  secondSwitchValueInput.value = temp;
+  // ^ most efficient way, one less variable created
+
+  //first = secondSwitchValueInput.value;
+  //second = firstSwitchValueInput.value;
+  // ^ but this doesnt?
+
+  //firstSwitchValueInput.value = secondSwitchValueInput.value;
+  //secondSwitchValueInput.value = firstSwitchValueInput.value;
+  // ^ or this?
+});
 
 // --^-- End of Task 3 --^--
