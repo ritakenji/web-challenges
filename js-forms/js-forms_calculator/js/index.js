bdbf4887d1840f70eac052f75ad308622785ebd6
Rita <rita.kenji@gmail.com>
2025-08-12 19:14:05 +0200

    finish calculator challenge

--- js-forms/js-forms_calculator/js/index.js
+++ js-forms/js-forms_calculator/js/index.js
@@ -23,9 +23,22 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let result;
-
   // --v-- write your code here --v--
+  console.log(event.target.elements.numberA.value);
 
+  let numberA = Number(event.target.elements.numberA.value);
+  let numberB = Number(event.target.elements.numberB.value);
+
+  if (event.target.elements.operator.value === "addition") {
+    result = add(numberA, numberB);
+  } else if (event.target.elements.operator.value === "subtraction") {
+    result = subtract(numberA, numberB);
+  } else if (event.target.elements.operator.value === "multiplication") {
+    result = multiply(numberA, numberB);
+  } else if (event.target.elements.operator.value === "division") {
+    result = divide(numberA, numberB);
+  }
+  event.target.reset();
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
