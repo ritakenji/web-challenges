426960a0e3e5951a7e3dbfa77229200843f57694
Rita <rita.kenji@gmail.com>
2025-08-19 12:17:41 +0200

    finish star wars console

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,14 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  console.log("response: ", response);
+  const data = await response.json(); // converting the data that the API sent us (JSON) into JS.
+  console.log("'data' object: ", data);
+  console.log("Value of object 'data': ", data.results);
+  console.log("Eye color of R2-D2: ", data.results[2]["eye_color"]);
+  return data;
+}
 
 fetchData();
