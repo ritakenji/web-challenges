a9028663b85aa034659d6d60b737962242cf54eb
Rita <rita.kenji@gmail.com>
2025-08-19 13:06:01 +0200

    correct challenge

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -6,10 +6,13 @@ async function fetchData() {
   const response = await fetch(url);
   console.log("response: ", response);
   const data = await response.json(); // converting the data that the API sent us (JSON) into JS.
-  console.log("'data' object: ", data);
-  console.log("Value of object 'data': ", data.results);
-  console.log("Eye color of R2-D2: ", data.results[2]["eye_color"]);
   return data;
 }
 
-fetchData();
+const character = await fetchData();
+
+console.log("I am here");
+console.log("'character' object: ", character);
+console.log("Values of object 'charater': ", character.results);
+console.log("Eye color of R2-D2: ", character.results[2]["eye_color"]); //for spaces and '-', use []
+console.log("Eye color of R2-D2: ", character.results[2].eye_color);
