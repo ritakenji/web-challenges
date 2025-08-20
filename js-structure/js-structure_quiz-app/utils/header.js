6c0f748ca2ad9a58866aabc77cf92a0e0dff584c
Rita Macedo <rita.kenji@gmail.com>
2025-08-20 14:59:35 +0200

    Revert "add new js files"

--- js-structure/js-structure_quiz-app/utils/header.js
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  const header = document.createElement("header");
-  header.classList.add("header");
-  header.innerHTML = /* html */ `
-	  <h1 class="header__title">Quiz-App</h1>
-	`;
-
-  return header;
-}
