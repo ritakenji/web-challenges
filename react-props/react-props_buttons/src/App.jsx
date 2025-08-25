9bb7efd0f48057ff71df4e0caf016bb38f91516a
Rita <rita.kenji@gmail.com>
2025-08-25 22:49:07 +0200

    finish buttons

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,21 +1,26 @@
 export default function App() {
+  function handleClick() {
+    console.log("You clicked me!");
+  }
+
   return (
     <Button
       text={"Click meee pleaseee, click meeeee!"}
       color={"#41acaf"}
       color2={"#752c71"}
-      disabled
+      onButton={handleClick}
     />
   );
 }
 
-function Button({ color, disabled, text, color2 }) {
+function Button({ color, disabled, text, color2, onButton }) {
   return (
     <>
       <button
         type="button"
         style={{ color: color, backgroundColor: color2 }}
         disabled={disabled}
+        onClick={onButton}
       >
         {text}
       </button>
