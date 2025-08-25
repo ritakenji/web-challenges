b75fa8700cd3c3d745d8beb11a84a1bdc0412a6a
Rita <rita.kenji@gmail.com>
2025-08-25 16:46:43 +0200

    finish smiley

--- react-props/react-props_smiley/src/App.jsx
+++ react-props/react-props_smiley/src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy={true} />;
+}
+
+function Smiley({ isHappy }) {
+  return <p>{isHappy ? "😃" : "😢"}</p>;
 }
