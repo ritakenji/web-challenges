9159ab521ecc7630c2bca6d2ff67e978abd547a4
Rita <rita.kenji@gmail.com>
2025-08-28 16:45:28 +0200

    finish colored number challenge

--- react-state-2/react-state-2_colored-number/src/components/Counter.jsx
+++ react-state-2/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import ColoredNumber from "./ColoredNumber";
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  /*   function handleIncrement() {
+    setValue(value + 1);
+  }*/
+
+  function handleDecrement() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={count} />
       <div className="counter__buttons-container">
         <button
+          onClick={() => setCount(count + 1)}
           type="button"
           className="counter__button"
           aria-label="increment count"
@@ -14,6 +26,7 @@ export default function Counter() {
           +
         </button>
         <button
+          onClick={handleDecrement} //i was writing onClick={handleDecrement()} which is swrong cause thats calling the function, we need to PASS it, not call
           type="button"
           className="counter__button"
           aria-label="decrement count"
