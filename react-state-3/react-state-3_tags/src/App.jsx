34ee8526c1fdfbaa1963b7c0659bb1500c7ab3ba
Rita <rita.kenji@gmail.com>
2025-08-29 13:10:20 +0200

    finish tags challenge

--- react-state-3/react-state-3_tags/src/App.jsx
+++ react-state-3/react-state-3_tags/src/App.jsx
@@ -6,10 +6,19 @@ import List from "./components/List";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    console.log("newTag here: ", newTag);
+    setTags([...tags, newTag]);
+  }
+
+  function handleDeleteTag(tagToDelete) {
+    setTags(tags.filter((tag) => tag != tagToDelete));
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
