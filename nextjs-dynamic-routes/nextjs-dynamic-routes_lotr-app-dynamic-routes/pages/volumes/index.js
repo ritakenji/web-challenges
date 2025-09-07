24a5de8ff38d8583d6188a053504f0536ecf03ed
Rita <rita.kenji@gmail.com>
2025-09-07 14:22:31 +0200

    add random volume button on index.js

--- nextjs-dynamic-routes/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
+++ nextjs-dynamic-routes/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -6,10 +6,21 @@
 
 import Head from "next/head";
 import Link from "next/link";
-import { introduction } from "@/lib/data";
-import { volumes } from "@/lib/data";
+import { introduction, volumes } from "@/lib/data";
+import { useRouter } from "next/router";
 
 export default function HomePage() {
+  const router = useRouter();
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function handleRandomVol() {
+    const randomVol = getRandomElement(volumes);
+    router.push(`/volumes/${randomVol.slug}`);
+  }
+
   return (
     <>
       <Head>
@@ -29,6 +40,7 @@ export default function HomePage() {
             </li>
           ))}
         </ul>
+        <button onClick={handleRandomVol}>Go to random volume</button>
       </main>
     </>
   );
