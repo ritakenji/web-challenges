afd5a9c87823c051698234394d4838ba0563bde9
Rita <rita.kenji@gmail.com>
2025-09-07 15:00:02 +0200

    replace default code with my code from previous lotr challenge (nextjs-dynamic-routes)

--- react-styled-components/react-styled-components_lotr-app-styling/pages/volumes/index.js
+++ react-styled-components/react-styled-components_lotr-app-styling/pages/volumes/index.js
@@ -1,18 +1,47 @@
-import Link from "next/link";
-import { introduction, volumes } from "../../lib/data";
+/* 
 
-export default function Volumes() {
+!!! CONTINUATION OF PREVIOUS LOTR'S CHALLENGE (NEXTJS-BASICS-AND-ROUTING), CODE BELOW WRITTEN BY ME !!!
+
+*/
+
+import Head from "next/head";
+import Link from "next/link";
+import { introduction, volumes } from "@/lib/data";
+import { useRouter } from "next/router";
+
+export default function HomePage() {
+  const router = useRouter();
+
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function handleRandomVol() {
+    const randomVol = getRandomElement(volumes);
+    router.push(`/volumes/${randomVol.slug}`);
+  }
+
   return (
     <>
-      <h1>The Lord of the Rings</h1>
-      <p>{introduction}</p>
-      <ul>
-        {volumes.map((volume) => (
-          <li key={volume.id}>
-            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <Head>
+        <title>LOTR</title>
+      </Head>
+      <main>
+        <h1>The Lord of The Rings</h1>
+        <p>{introduction}</p>
+
+        <h2>All Volumes</h2>
+        <ul>
+          {volumes.map((volume, index) => (
+            <li key={index}>
+              <Link href={`/volumes/${volume.slug.toLocaleLowerCase()}`}>
+                {volume.title}
+              </Link>{" "}
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleRandomVol}>Go to random volume</button>
+      </main>
     </>
   );
 }
