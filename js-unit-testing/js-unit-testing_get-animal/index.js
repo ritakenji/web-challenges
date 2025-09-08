e4545ee3491f625d591476f860324cf16ebf3abd
Rita <rita.kenji@gmail.com>
2025-09-08 11:28:03 +0200

    finish animals chalenge

--- js-unit-testing/js-unit-testing_get-animal/index.js
+++ js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (!animal) {
+    return "I do not like animals at all!";
+  } else if (animal === "cats") {
+    return `I totally love cats!`;
+  } else {
+    return `I like ${animal}!`;
+  }
+}
