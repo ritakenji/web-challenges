a67b48bd8cf40b58a5acb54feb626d85af385cfc
Rita <rita.kenji@gmail.com>
2025-09-18 17:27:44 +0200

    finish challenge

--- backend-create/backend-create_products/pages/api/products/index.js
+++ backend-create/backend-create_products/pages/api/products/index.js
@@ -11,5 +11,15 @@ export default async function handler(request, response) {
     return;
   }
 
+  if (request.method === "POST") {
+    console.log("In the POST");
+    // console.log('request', request);
+    const productData = request.body;
+    await Product.create(productData);
+
+    // return response.status(200).end();
+    return response.status(201).json({ status: "Product created." });
+  }
+
   response.status(405).json({ status: "Method not allowed." });
 }
